4228a6ef0f86e52d7a3ef4f27b6ec5d750bf1041
rostj <rostj@msoe.edu>
2025-11-18 16:24:54 -0600

    Update CAN debug prints; TX message flow updated correctly along with RX message code implemented.

--- ped_box/Core/Src/main.c
+++ ped_box/Core/Src/main.c
@@ -151,92 +151,120 @@ int main(void)
 
   // Start ADC
   HAL_ADC_Start(&hadc1);  // Start the ADC
-  HAL_FDCAN_Start(&hfdcan1);  // Start FDCAN1 communication
+
+  // Configure CAN filter to accept all standard frames
+  // FDCAN will not receive ANYTHING without this filter
+  FDCAN_FilterTypeDef sFilterConfig = {0};
+  sFilterConfig.IdType = FDCAN_STANDARD_ID;
+  sFilterConfig.FilterIndex = 0;
+  sFilterConfig.FilterType = FDCAN_FILTER_RANGE;
+  sFilterConfig.FilterConfig = FDCAN_FILTER_TO_RXFIFO0;
+  sFilterConfig.FilterID1 = 0x000;
+  sFilterConfig.FilterID2 = 0x7FF;  // Accept ALL IDs
+  HAL_FDCAN_ConfigFilter(&hfdcan1, &sFilterConfig);
+
+  // Start FDCAN1 communication
+  HAL_FDCAN_Start(&hfdcan1);
+
+  // Enable receive interrupts for CAN
+  // when new CAN frame arrives, hardware triggers interrupt
+  HAL_FDCAN_ActivateNotification(&hfdcan1,
+      FDCAN_IT_RX_FIFO0_NEW_MESSAGE,
+      0);
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  HAL_ADC_Start(&hadc1);  // Start the ADC
-
   while (1)
   {
 	  // declare variables
 	  uint32_t adcValue1;
 	  uint32_t adcValue2;
+
       // Configure and read from ADC1_IN8 (Pot 1)
-    if (HAL_ADC_PollForConversion(&hadc1, 1000000) == HAL_OK)
-    {
-            adcValue1 = HAL_ADC_GetValue(&hadc1);
-    }
+      if (HAL_ADC_PollForConversion(&hadc1, 1000000) == HAL_OK)
+      {
+          adcValue1 = HAL_ADC_GetValue(&hadc1);
+      }
 
+      // Stop ADC to reconfigure for channel 9
+      HAL_ADC_Stop(&hadc1);
 
-            // Stop the ADC and configure it for ADC1_IN9 (Pot 2)
-            HAL_ADC_Stop(&hadc1);
+      ADC_ChannelConfTypeDef sConfig = {0};
+      sConfig.Channel = ADC_CHANNEL_9; // ADC1_IN9
+      sConfig.Rank = ADC_REGULAR_RANK_1;
+      sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES_5;
+      sConfig.SingleDiff = ADC_SINGLE_ENDED;
+      HAL_ADC_ConfigChannel(&hadc1, &sConfig);
 
-            ADC_ChannelConfTypeDef sConfig = {0};
-            sConfig.Channel = ADC_CHANNEL_9; // ADC1_IN9
-            sConfig.Rank = ADC_REGULAR_RANK_1;
-            sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES_5;
-            sConfig.SingleDiff = ADC_SINGLE_ENDED;
-            HAL_ADC_ConfigChannel(&hadc1, &sConfig);
+      // Start conversion for ADC1_IN9
+      HAL_ADC_Start(&hadc1);
+      if (HAL_ADC_PollForConversion(&hadc1, 1000000) == HAL_OK)
+      {
+          adcValue2 = HAL_ADC_GetValue(&hadc1);
 
-            // Start ADC conversion for ADC1_IN9 (Pot 2)
-            HAL_ADC_Start(&hadc1);
-            if (HAL_ADC_PollForConversion(&hadc1, 1000000) == HAL_OK)
-            {
-            //	uint32_t adcValue2;
-                adcValue2 = HAL_ADC_GetValue(&hadc1); // Pot 2 value
+          // inverting pot 2 value (assuming it's wired backwards)
+          adcValue2 = 4095 - adcValue2;
+      }
 
-                // inverting pot 2 value (assuming it's wired backwards)
-                adcValue2 = 4095 - adcValue2;
-            }
+      // Convert ADC values to percentages
+      float percentage1 = ((float)adcValue1 / 4095) * 100;
+      float percentage2 = ((float)adcValue2 / 4095) * 100;
 
-            // Convert ADC values to percentage
-            float percentage1;
-            float percentage2;
+      // check if percentages are off?
+      if ((percentage1 >= percentage2 + 5) || (percentage1 <= percentage2 - 5))
+      {
+          Error_Handler();
+      }
 
-            percentage1 = ((float)adcValue1 / 4095) * 100;  // For ADC1_IN8 (PC2)
-            percentage2 = ((float)adcValue2 / 4095) * 100;  // For ADC1_IN9 (PC3)
+      // output to CAN
+      FDCAN_TxHeaderTypeDef TxHeader;
+      uint8_t canData;  // CAN data payload (one byte)
 
+      // CAN frame setup
+      // Added all required fields
+      TxHeader.Identifier = 0x321;  // CAN identifier (can be any valid identifier)
+      // standard 11 bit CAN ID
+      TxHeader.IdType = FDCAN_STANDARD_ID;
+      // sending a normal data frame (we want to send the pedal percentage)
+      TxHeader.TxFrameType = FDCAN_DATA_FRAME;
+      // frame contains 1 byte. We are sending a single % value converted to uint8_t
+      TxHeader.DataLength = FDCAN_DLC_BYTES_1;
+      // indicates node error state in the frame.
+      TxHeader.ErrorStateIndicator = FDCAN_ESI_ACTIVE;
+      // since we are using classic CAN, bitrate switching is off
+      TxHeader.BitRateSwitch = FDCAN_BRS_OFF;
+      // automotive pedal sensors typically use classical CAN
+      TxHeader.FDFormat = FDCAN_CLASSIC_CAN;
+      // we don't need time stamps or diagnostic info
+      TxHeader.TxEventFifoControl = FDCAN_NO_TX_EVENTS;
+      // set to 0 because we aren't using TX event features
+      TxHeader.MessageMarker = 0;
+      // not needed since we aren't using time stamps
+      TxHeader.TransmitGlobalTime = DISABLE;
 
-            // check if percentages are off?
-            if((percentage1 >= percentage2 + 5) || (percentage1 <= percentage2 - 5))
-            {
-            	// call error message?
-            	// Error_Handler();  ? Maybe???
-            }
+      // Prepare data for CAN: Convert percentage1 to 8-bit value
+      int8_t percentage1Int = (int8_t)(percentage1 * 1);
 
+      // Load data
+      canData = percentage1Int;
 
-     /*       // output to CAN
-            FDCAN_TxHeaderTypeDef TxHeader;
-            uint8_t canData;  // CAN data payload (one byte)
+      // print when we send a CAN message
+      printf("TX CAN: ID=0x%03X, Data=%d\r\n", TxHeader.Identifier, canData);
 
-            // CAN frame setup
-            TxHeader.Identifier = 0x321;  // CAN identifier (can be any valid identifier)
-            TxHeader.DLC = 1;  // Data length code (DLC), sending 1 byte
-            TxHeader.TransmitGlobalTime = DISABLE;  // No global time
+      // Send the CAN message
+      if (HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &TxHeader, &canData) != HAL_OK)
+      {
+          Error_Handler();
+      }
 
-            // Prepare data for CAN: Convert percentage1 to an 8-bit integer
-            int8_t percentage1Int = (int8_t)(percentage1 * 100);  // Convert percentage1 to an integer (range: -128 to 127)
+      printf("Percentage 1: %.2f%%, Percentage 2: %.2f%%\r\n", percentage1, percentage2);
 
-            // Load the CAN data
-            canData = percentage1Int;  // Store the 8-bit integer value of percentage1 in canData
+  }
+  /* USER CODE END WHILE */
 
-            // Send the CAN message
-            if (HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &TxHeader, &canData) != HAL_OK)
-            {
-                Error_Handler();  // Handle error if CAN message can't be added to FIFO
-            }
-	*/
-
-            // output to LPUART -- No clue
-            printf("Percentage 1: %.2f%%, Percentage 2: %.2f%%\r\n", percentage1, percentage2);
-
-        }
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
+  /* USER CODE BEGIN 3 */
   return 0;
   /* USER CODE END 3 */
 }
@@ -258,8 +286,8 @@ void SystemClock_Config(void)
   * in the RCC_OscInitTypeDef structure.
   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.HSEState = HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV2;
   RCC_OscInitStruct.PLL.PLLN = 8;
@@ -286,6 +314,28 @@ void SystemClock_Config(void)
   }
 }
 
+/* CAN RX Callback */
+// prints out every CAN message that is received
+void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo0ITs)
+{
+	// did the interrupt happen because a new CAN frame arrived
+    if (RxFifo0ITs & FDCAN_IT_RX_FIFO0_NEW_MESSAGE)
+    {
+    	// create new structs for the received frame
+    	// header struct
+        FDCAN_RxHeaderTypeDef RxHeader;
+        // data payload struct
+        uint8_t rxData[8];
+
+        // removes the oldest CAN frame, writes new info into structs
+        HAL_FDCAN_GetRxMessage(hfdcan, FDCAN_RX_FIFO0, &RxHeader, rxData);
+
+        // prints the CAN ID, and the first byte of the payload
+        printf("RX CAN: ID=0x%03lX, Data=%d\r\n",
+               RxHeader.Identifier, rxData[0]);
+    }
+}
+
 /* USER CODE BEGIN 4 */
 
 /* USER CODE END 4 */
@@ -297,7 +347,6 @@ void SystemClock_Config(void)
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
   while (1)
   {
@@ -305,19 +354,15 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
   * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
   * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
+  * @param  line: assert_param error line source #
   */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
